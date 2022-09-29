562132f009b68b7f3f740ebac080b12b0541b750
Vance97 <vansmuchongo@gmail.com>
2022-09-29 21:59:13 +0200

    InputGenerator working

--- sorts.cpp
+++ sorts.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 using namespace std;
 
+/*
+@ Function that accepts an integer N, the size of the input, and returns a vector of size N.
+@ Where each element is a random integer uniformly drawn from the range 1 to 1000000.
+*/
+
+vector<int>InputGenerator(int N){
+
+    //vector to be returned
+    vector<int> GeneratedElements;
+
+    for(int i = 1;i<=N;i++){
+        int RandomNumber = 1 + (rand() % 1000);
+        GeneratedElements.push_back(RandomNumber);
+    }
+
+    return GeneratedElements;
+
+}
+
 int main(){
-    cout<<"Hello, World!"<<endl;
+    cout<<"Enter the array size"<<endl;
+    int N;
+    cin>>N;
+    vector<int> Result =  InputGenerator(N);
+    for(int Number: Result){
+        cout << Number <<", ";
+    }
     return 0;
 }
